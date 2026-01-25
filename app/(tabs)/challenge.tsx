e85f6492cc25daa5bd984479f9a5726e61c56628
lup202311 <lucie.phung@etu.u-paris.fr>
2026-01-25 21:23:29 +0100

    modif page défis

--- app/(tabs)/challenge.tsx
+++ app/(tabs)/challenge.tsx
@@ -1,4 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, FlatList, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { database } from "../firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+
+//permet définir défis avec ces caractéristiques à chaque fois qu'on utilise
+interface Defis {
+  id:number;
+  categorie:string[];
+  co2:number;
+  description: string;
+  difficulte: number;
+  image: string;
+  nom: string;
+  validation: boolean;
+}
+
+//stocker la liste de défis
+const ListDefis = () => {
+  const[]
+}
 
 export default function Challenge() {
   return (
