76f69a92eea42ea53159888d5b098eeff759e4a4
lup202311 <lucie.phung@etu.u-paris.fr>
2026-03-20 23:50:53 +0100

    Ajout Notifications

---
+++ app/(notifs)/getNotifications.tsx
@@ -0,0 +1,7 @@
+import * as Notifications from 'expo-notifications';
+import { Platform } from 'react-native';
+import * as Device from 'expo-device';
+
+export default function getNotifications() {
+    const handleP
+}
