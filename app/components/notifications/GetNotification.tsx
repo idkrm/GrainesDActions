54986392f9b9eb92d54847c2891b73b881c236ef
lup202311 <lucie.phung@etu.u-paris.fr>
2026-03-23 11:38:11 +0100

    Modification de la fonctionnalité notification v6

--- app/components/notifications/GetNotification.tsx
+++ app/components/notifications/GetNotification.tsx
@@ -15,13 +15,14 @@ export const envoyerNotification = async () => {
     // Infos de la notif
     await Notifications.scheduleNotificationAsync({
         content: {
-            title: "Un nouveau défi vous attend ! ⏰",
+            title: "Vous n'avez pas de défis en cours ! ⏰",
             body: "Contribuez à la planète en participant aux défis 🌱",
             data: { screen: 'index'}
         },
         trigger: {
             type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
-            seconds: 10,
+            seconds: 60,
+            repeats: true
         }
     })
 
